62fedfc6de9268b0284e801bf699a3cc42c610ef
Jesse Ward <jesse@jesseward.net>
2022-10-31 21:17:27 -0700

    Reorganize using statements

--- src/AutoExtractGenes.cs
+++ src/AutoExtractGenes.cs
@@ -1,17 +1,10 @@
-﻿namespace AutoExtractGenes
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Reflection;
-    using System.Reflection.Emit;
-    using HarmonyLib;
-    using JetBrains.Annotations;
-    using RimWorld;
-    using RimWorld.Planet;
-    using UnityEngine;
-    using Verse;
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using Verse;
 
+namespace AutoExtractGenes
+{
     public class AutoExtractGenes : Mod
     {
         private static int reorderableGroupId = -1;
