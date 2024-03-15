2c587d06f12c73ca4d2a51c19180122ac885aabb
Jesse Ward <jesse@jesseward.net>
2024-03-15 04:34:48 -0700

    Updated for Rimworld 1.5

--- src/HealthCardUtilityPatches.cs
+++ src/HealthCardUtilityPatches.cs
@@ -1,56 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Verse;
+﻿using HarmonyLib;
 using RimWorld;
-using HarmonyLib;
 using UnityEngine;
-
+using Verse;
 
 namespace AutoExtractGenes
 {
-	[HarmonyPatch(typeof(HealthCardUtility))]
-	[HarmonyPatch("DrawOverviewTab")]
-	public class HealthCardUtilityPatch_DrawOverViewTab
-	{
-		public static void Postfix(ref float __result, ref Rect leftRect, ref Pawn pawn)
-		{
-			var curY = __result;
+    [HarmonyPatch(typeof(HealthCardUtility))]
+    [HarmonyPatch("DrawOverviewTab")]
+    public class HealthCardUtilityPatch_DrawOverViewTab
+    {
+        public static void Postfix(ref float __result, ref Rect rect, ref Pawn pawn, ref float curY)
+        {
+            var retCurY = __result;
 
-			var autoExtractGenesComp = Utils.GetAutoExtractGenesComponent(pawn);
+            var autoExtractGenesComp = Utils.GetAutoExtractGenesComponent(pawn);
 
-			if (autoExtractGenesComp == null)
-				return;
+            if (autoExtractGenesComp == null)
+                return;
 
-			Text.Font = GameFont.Tiny;
-			Text.Anchor = TextAnchor.UpperLeft;
-			GUI.color = Color.white;
+            Text.Font = GameFont.Tiny;
+            Text.Anchor = TextAnchor.UpperLeft;
+            GUI.color = Color.white;
 
-			//curY += 4f;
+            //retCurY += 4f;
 
-			var rect = new Rect(0.0f, curY, leftRect.width, 24f);
-			Widgets.CheckboxLabeled(rect, (string)"nibato.AutoExtractGenes.AutoExtractGenes".Translate(), ref autoExtractGenesComp.isEnabled);
+            var newRect = new Rect(0.0f, retCurY, rect.width, 24f);
+            Widgets.CheckboxLabeled(newRect, (string)"nibato.AutoExtractGenes.AutoExtractGenes".Translate(), ref autoExtractGenesComp.isEnabled);
 
-			if (Mouse.IsOver(rect))
-				TooltipHandler.TipRegion(rect, "nibato.AutoExtractGenes.AutoExtractGenes.Tooltip".Translate());
+            if (Mouse.IsOver(newRect))
+                TooltipHandler.TipRegion(newRect, "nibato.AutoExtractGenes.AutoExtractGenes.Tooltip".Translate());
 
-			curY += 28f;
-			__result = curY;
-		}
-	}
+            retCurY += 28f;
+            __result = retCurY;
+        }
+    }
 
+    [HarmonyPatch(typeof(ITab_Pawn_Health), MethodType.Constructor)]
+    public class HealthCardUtilityPatch_ITab_Pawn_Health
+    {
+        private static AccessTools.FieldRef<ITab_Pawn_Health, Vector2> size = AccessTools.FieldRefAccess<ITab_Pawn_Health, Vector2>("size");
 
-
-	[HarmonyPatch(typeof(ITab_Pawn_Health), MethodType.Constructor)]
-	public class HealthCardUtilityPatch_ITab_Pawn_Health
-	{
-		private static AccessTools.FieldRef<ITab_Pawn_Health, Vector2> size = AccessTools.FieldRefAccess<ITab_Pawn_Health, Vector2>("size");
-
-
-		public static void Postfix(ITab_Pawn_Health __instance)
-		{
-			size(__instance).y += 28f;
-		}
-	}
+        public static void Postfix(ITab_Pawn_Health __instance)
+        {
+            size(__instance).y += 28f;
+        }
+    }
 }
